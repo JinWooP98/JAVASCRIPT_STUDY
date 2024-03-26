a39a8cba28fe58dd8f343287879ae3ffbdb9c0bd
JinWooP98 <qwdk0406@naver.com>
2024-03-26 23:26:57 +0900

    # JS-study Day5_3

---
+++ 1. core javascript/6. 함수/7. quiz3.js
@@ -0,0 +1,34 @@
+//result에는 56이 리턴되어야 함.
+var result1 = min2(134, 56);
+console.log(`result1: ${result1}`);
+
+// 셋중에 가장 작은 수 50이 result에 저장되어야 함.
+var result2 = min3(120, 50, 99);
+console.log(`result2: ${result2}`);
+
+
+// 숫자 1개를 전달하면 해당 숫자가 짝수인지 판별하는 함수
+// 24를 전달하면 true리턴, 홀수를 전달하면 false리턴, 
+// 단, 0은 짝수취급
+var result3 = isEven(3);
+console.log(`result3: ${result3}`);
+
+function min2(x, y) {
+  if( x > y) {
+    return y;
+  } else {
+    return x;
+  }
+}
+
+function min3(x, y, z) {
+  var minNum = x;
+  if (minNum > y) minNum = y;
+  if (minNum > z) minNum = z;
+  return minNum;
+}
+
+function isEven(n) {
+  if (n === 0) return true;
+  return (n % 2 === 0);
+}
